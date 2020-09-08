572dedc484c277c44c953153ccde7310a112d2fb
elenanikolova190 <70112811+elenanikolova190@users.noreply.github.com>
2020-09-08 22:47:35 +0100

    Completed Excercise 001

--- src/main/java/com/techreturners/exercise001/Exercise001.java
+++ src/main/java/com/techreturners/exercise001/Exercise001.java
@@ -1,26 +1,48 @@
 package com.techreturners.exercise001;
 
 import java.util.List;
+import java.lang.Math;
+
 
 public class Exercise001 {
-    public String capitalizeWord(String word) {
-        // Add your code here
-        return "";
+    public String capitalizeWord(final String word) {
+        final String cap = word.substring(0, 1).toUpperCase() + word.substring(1);
+        return cap;
     }
 
-    public String generateInitials(String firstName, String lastName) {
-        return "";
+    public String generateInitials(final String firstName, final String lastName) {
+        final String initials = firstName.substring(0, 1).toUpperCase() + "." + lastName.substring(0, 1).toUpperCase();
+        return initials;
     }
 
-    public double addVat(double originalPrice, double vatRate) {
-        return 0.0;
+    public double addVat(final double originalPrice, final double vatRate) {
+        final double finalPrice = originalPrice + originalPrice * vatRate / 100;
+        return Math.floor(finalPrice*100)/100;
     }
 
-    public String reverse(String sentence) {
-        return "";
+    public String reverse(final String sentence) {
+        char[] tempArray = sentence.toCharArray();
+        int left, right = 0;
+        right = tempArray.length - 1;
+
+        for(left = 0; left < right; left++, right--){
+            char temp = tempArray[left];
+            tempArray[left] = tempArray[right];
+            tempArray[right] = temp;
+        }
+
+        String str = new String(tempArray);
+        return str;
     }
 
-    public int countLinuxUsers(List<User> users) {
-        return 0;
+    public int countLinuxUsers(final List<User> users) {
+        int occurrences = 0;
+        
+        for(User user : users){
+            if(user.getType().equals("Linux")){
+                occurrences++;
+            }
+        }
+        return occurrences;
     }
 }
