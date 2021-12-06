5d40a81ad7bcfdeb7af2394d906794329d8708e1
CleideS <40967363+cleides@users.noreply.github.com>
2021-12-06 17:48:21 +0000

    Exercise001 partial

--- src/main/java/com/techreturners/exercise001/Exercise001.java
+++ src/main/java/com/techreturners/exercise001/Exercise001.java
@@ -1,21 +1,26 @@
 package com.techreturners.exercise001;
 
 import java.util.List;
+import java.util.Locale;
 
 public class Exercise001 {
     public String capitalizeWord(String word) {
-        // Add your code here
-        return "";
+        String result = word.substring(0, 1).toUpperCase() + word.substring(1);
+        return result;
     }
 
     public String generateInitials(String firstName, String lastName) {
-        // Add your code here
-        return "";
+        String firstNameInit = firstName.substring(0, 1);
+        String lastNameInit = lastName.substring(0, 1);
+        String initials = firstNameInit + "." + lastNameInit;
+        return initials;
     }
 
     public double addVat(double originalPrice, double vatRate) {
-        // Add your code here
-        return 0.0;
+        double result = (originalPrice + (originalPrice * vatRate / 100.0d));
+        //round to 2 decimal places
+        result = (double) Math.round(result * 100.0d) / 100.0d;
+        return result;
     }
 
     public String reverse(String sentence) {
