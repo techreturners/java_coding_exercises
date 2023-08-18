49156c0948fc48ced2ed3a19a36eba7a0506c192
Amjad <amjadshakir@gmail.com>
2023-08-18 15:29:13 +0100

    fix for Exercise001Test#checkHelloAlreadyUppercase

--- src/test/java/com/techreturners/exercise001/Exercise001Test.java
+++ src/test/java/com/techreturners/exercise001/Exercise001Test.java
@@ -18,8 +18,7 @@ public class Exercise001Test {
     }
 
     @Test
-    @Disabled
-    public void checkHelloAlreadyUppercase() {
+        public void checkHelloAlreadyUppercase() {
         Exercise001 ex001 = new Exercise001();
         assertEquals("Hello", ex001.capitalizeWord("Hello"));
     }
