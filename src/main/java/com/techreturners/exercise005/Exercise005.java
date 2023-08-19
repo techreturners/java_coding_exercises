cec0b15d11bbb6d06f37d45f42053c6d865398c5
Amjad <amjadshakir@gmail.com>
2023-08-19 23:06:46 +0100

    fix for Exercise005Test

--- src/main/java/com/techreturners/exercise005/Exercise005.java
+++ src/main/java/com/techreturners/exercise005/Exercise005.java
@@ -1,9 +1,21 @@
 package com.techreturners.exercise005;
 
+import java.util.*;
+
 public class Exercise005 {
 
     public boolean isPangram(String input) {
-        throw new UnsupportedOperationException("You can delete this statement and add your code here.");
+//            input = input.toLowerCase();
+//            List<Character> list = new ArrayList<>();
+//        for (int i=0 ; i< input.length(); i++){
+//            if (Character.isLetter(input.charAt(i))){
+//                list.add(input.charAt(i));
+//            }
+//        }
+//        return list.stream().distinct().count() ==  26;
+
+    return  input.chars().map(Character::toLowerCase).filter(Character::isLetter).distinct().count() ==  26;
+
     }
 
 }
